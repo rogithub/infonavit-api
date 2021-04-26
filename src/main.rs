189b59c64ee7789182fad053e4ef94363c01a466
rodrigo debian <correo.rodrigo@gmail.com>
2021-04-26 13:34:38 -0500

    templates config

--- src/main.rs
+++ src/main.rs
@@ -1,7 +1,21 @@
 #![feature(proc_macro_hygiene, decl_macro)]
 
-#[macro_use] extern crate rocket;
+#[macro_use]
+extern crate rocket;
+use rocket_contrib::templates::Template;
+
+use std::collections::HashMap;
+
+#[get("/")]
+fn index() -> Template {
+    let mut context: HashMap<&str, i32> = HashMap::new();
+    context.insert("test", 1);
+    Template::render("index", &context)
+}
 
 fn main() {
-    rocket::ignite().launch();
+    rocket::ignite()
+        .mount("/", routes![index])
+        .attach(Template::fairing())
+        .launch();
 }
